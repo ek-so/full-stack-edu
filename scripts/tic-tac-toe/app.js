aea8e0149c36895b602424dda8fab7de33ffd686
ek-so <eksomail@gmail.com>
2024-04-26 08:50:59 +0200

    Set users names

--- scripts/tic-tac-toe/app.js
+++ scripts/tic-tac-toe/app.js
@@ -1,11 +1,29 @@
+let editedPlayer = 0;
+
+const players = [
+  {
+    name: "",
+    symbol: "X",
+  },
+  {
+    name: "",
+    symbol: "O",
+  },
+];
+
 const backdropElement = document.getElementById("backdrop");
 const playerConfigOverlay = document.getElementById("modal-inner");
+const formElement = document.querySelector("form");
+const errorsOutputElement = document.getElementById("config-errors");
 
-const editPlayerOneButton = document.getElementById("edit-player-one");
-const editPlayerTwoButton = document.getElementById("edit-player-two");
+const editPlayerOneButton = document.getElementById("edit-player-1");
+const editPlayerTwoButton = document.getElementById("edit-player-2");
 const cancelConfigElement = document.getElementById("cancel");
 
 editPlayerOneButton.addEventListener("click", openPlayerConfig);
 editPlayerTwoButton.addEventListener("click", openPlayerConfig);
+
 cancelConfigElement.addEventListener("click", closePlayerConfig);
 backdropElement.addEventListener("click", closePlayerConfig);
+
+formElement.addEventListener("submit", savePlayerConfig); //when form is submitted
