c7889cb2ef71a3e729be6367c710c21556a4ccc3
ek-so <eksomail@gmail.com>
2024-05-03 12:15:37 +0200

    dynamic links and error handling

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const express = require('express');
+const uuid = require('uuid');
 
 const app = express();
 
@@ -27,12 +28,29 @@ app.get('/restaurants', function (req, res) {
   });
 });
 
+app.get('/restaurants/:id', function (req, res) {
+  const restaurantId = req.params.id;
+  const filePath = path.join(__dirname, 'data', 'restaurants.json');
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  for (const restaurant of storedRestaurants) {
+    if (restaurant.id === restaurantId) {
+      return res.render('restaurant-detail', { restaurant: restaurant });
+    }
+  }
+
+  res.render('404');
+});
+
 app.get('/recommend', function (req, res) {
   res.render('recommend');
 });
 
 app.post('/recommend', function (req, res) {
   const restaurant = req.body;
+  restaurant.id = uuid.v4();
   const filePath = path.join(__dirname, 'data', 'restaurants.json');
 
   const fileData = fs.readFileSync(filePath);
@@ -51,6 +69,14 @@ app.get('/confirm', function (req, res) {
 
 app.get('/about', function (req, res) {
   res.render('about');
+});
+
+app.use(function(req, res) {
+    res.render('404');
+}); //important to add at the end, if nothing was handled by code above
+
+app.use(function(error, req, res, next) {
+    res.render('500');
 });
 
 app.listen(3000);
