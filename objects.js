f909e1bc83970584ef4d94070299cc03edd96e08
ek-so <eksomail@gmail.com>
2024-05-04 11:35:54 +0200

    completed the chapter about advanced JS concepts

--- objects.js
+++ objects.js
@@ -16,3 +16,29 @@ class Job {
 
 const developer = new Job("Developer", "NY", 50000); //utilize blueprint to create an object
 console.log(developer);
+
+
+class Job2 {
+    constructor(jobTitle, place, salary) {
+      //adds method to a class, tells how to constract object
+      this.title = jobTitle; //this refers to the future object to be created
+      this.location = place;
+      this.salary = salary;
+    }
+    describe () {
+        console.log(`I'm a ${this.title}, I work in ${this.location} and I earn ${this.salary}.`);
+    }//method adding
+  };
+
+const cook = new Job2("cook", "LA", 10000); //utilize blueprint to create an object
+cook.describe(); //manually added method
+
+const input = ['Kate', 'Sossedova'];
+const firstName = input[0];
+const lastName = input[1];
+const [first, last] = input; //destructure array; pulls vaues our of array, equvivalent to const first = input[0] etc.
+console.log(first);
+
+const { title: jTitle, location: jLocation } = job; //destructure object
+//jTitle = job.title; //gives same result
+console.log(jTitle);
