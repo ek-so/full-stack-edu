863aa8aff32ad684ee75c44ac2463472f6e4cd85
ek-so <eksomail@gmail.com>
2024-05-15 08:12:26 +0200

    basic styling

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const authRoutes = require('./routes/auth.routes');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views')); //dirname is available thanks to require path
 
-app.use(express.static('public'));
+app.use(express.static('public')); //we serve this folder separately for the user
 
 app.use(authRoutes);
 
