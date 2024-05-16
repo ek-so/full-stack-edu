d4cdad92e08cea820caf40b340c622e3c2e4fe73
ek-so <eksomail@gmail.com>
2024-05-16 12:32:12 +0200

    established connection to mongodb

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const authRoutes = require('./routes/auth.routes');
+const db = require("./data/database");
+
+const authRoutes = require("./routes/auth.routes");
+const database = require("./data/database");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views')); //dirname is available thanks to require path
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views")); //dirname is available thanks to require path
 
-app.use(express.static('public')); //we serve this folder separately for the user
+app.use(express.static("public")); //we serve this folder separately for the user
 
 app.use(authRoutes);
 
-app.listen(3000);
+db.connectToDatabase()
+  .then(function () {
+    app.listen(3000);
+    console.log("Connected to DB")
+  })
+  .catch(function (error) {
+    console.log("Failed to connect to DB");
+    console.log(error);
+  });
