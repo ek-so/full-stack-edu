9bc80ad67f7fda235fd747a819b182224d4343e7
ek-so <eksomail@gmail.com>
2024-05-16 16:19:17 +0200

    handling sessions storage

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const path = require("path");
 
 const express = require("express");
+const csrf = require("csurf");
+const expressSession = require('express-session');
 
+const createSessionConfig = require('./config/session');
 const db = require("./data/database");
-
+const addCsrfTokenMiddleware = require('./middlewares/csrf-token');
+const errorHandlerMiddleware = require("./middlewares/error-handler");
 const authRoutes = require("./routes/auth.routes");
 
 const app = express();
@@ -14,7 +18,16 @@ app.set("views", path.join(__dirname, "views")); //dirname is available thanks t
 app.use(express.static("public")); //we serve this folder separately for the user
 app.use(express.urlencoded({ extended: false })); //built-in middleware function in Express. It parses incoming requests with URL-encoded payloads and is based on a body parser.
 
+const sessionConfig = createSessionConfig();
+
+app.use(expressSession(sessionConfig));
+app.use(csrf()); //need to be done before request reaches routes; checks that all requests have tokens
+
+app.use(addCsrfTokenMiddleware); //need to be added after csrf because it relies on locals that are included there into a session
+
 app.use(authRoutes);
+
+app.use(errorHandlerMiddleware);
 
 db.connectToDatabase()
   .then(function () {
---
+++ config/session.js
@@ -0,0 +1,29 @@
+const expressSession = require("express-session");
+const mongoDbStore = require("connect-mongodb-session");
+  
+
+function createSessionStore() {
+  const MongoDBStore = mongoDbStore(expressSession);
+
+  const store = new MongoDBStore({
+    uri: "mongodb+srv://ekso:0VynB3Src65Nv8vj@cluster0.nybop9t.mongodb.net/?retryWrites=true&w=majority",
+    databaseName: "100-days-of-code",
+    collection: "sessions",
+  });
+
+  return store;
+}
+
+function createSessionConfig() {
+  return {
+    secret: "super-secret",
+    resave: false,
+    saveUninitialized: false,
+    store: createSessionStore(),
+    cookie: {
+      maxAge: 2 * 24 * 60 * 60 * 1000,
+    },
+  };
+}
+
+module.exports = createSessionConfig;
