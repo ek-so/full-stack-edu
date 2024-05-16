ef2d0b08c7faad92cffe986d15f199960ec6eb53
ek-so <eksomail@gmail.com>
2024-05-16 15:19:02 +0200

    signup

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const express = require("express");
 const db = require("./data/database");
 
 const authRoutes = require("./routes/auth.routes");
-const database = require("./data/database");
 
 const app = express();
 
@@ -13,13 +12,14 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views")); //dirname is available thanks to require path
 
 app.use(express.static("public")); //we serve this folder separately for the user
+app.use(express.urlencoded({ extended: false })); //built-in middleware function in Express. It parses incoming requests with URL-encoded payloads and is based on a body parser.
 
 app.use(authRoutes);
 
 db.connectToDatabase()
   .then(function () {
     app.listen(3000);
-    console.log("Connected to DB")
+    console.log("Connected to DB");
   })
   .catch(function (error) {
     console.log("Failed to connect to DB");
