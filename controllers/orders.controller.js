4913be615dc8cb325751f952be90912a672ec705
ek-so <eksomail@gmail.com>
2024-05-21 09:07:49 +0200

    stripe payment

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -1,3 +1,5 @@
+const stripe = require('stripe')('sk_test_51PIlmMDl3hjL4R5a10uHawlmpgSCKOOlu7rwByCgA7B4j7csZFjjRRYwkxXQmPAsillQ7gb3GM5jhqGJE7cwWI5a00UHTbHHPg');
+
 const Order = require('../models/order.model');
 const User = require('../models/user.model');
 
@@ -5,7 +7,7 @@ async function getOrders(req, res) {
   try {
     const orders = await Order.findAllForUser(res.locals.uid);
     res.render('customer/orders/all-orders', {
-      orders: orders
+      orders: orders,
     });
   } catch (error) {
     next(error);
@@ -33,10 +35,39 @@ async function addOrder(req, res, next) {
 
   req.session.cart = null;
 
-  res.redirect('/orders');
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: cart.items.map(function(item) {
+      return  {
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: item.product.title
+          },
+          unit_amount: +item.product.price.toFixed(2) * 100
+        },
+        quantity: item.quantity,
+      }
+    }),
+    mode: 'payment',
+    success_url: `http://localhost:3000/orders/success`,
+    cancel_url: `http://localhost:3000/orders/failure`,
+  });
+
+  res.redirect(303, session.url);
+}
+
+function getSuccess(req, res) {
+  res.render('customer/orders/success');
+}
+
+function getFailure(req, res) {
+  res.render('customer/orders/failure');
 }
 
 module.exports = {
   addOrder: addOrder,
   getOrders: getOrders,
+  getSuccess: getSuccess,
+  getFailure: getFailure
 };
