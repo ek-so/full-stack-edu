6e4a484dd2db1eaf5d6bc3895161d09dd1e1f315
ek-so <eksomail@gmail.com>
2024-05-21 10:50:03 +0200

    static site delpoyment

--- dynamic-site/data/database.js
+++ dynamic-site/data/database.js
@@ -2,10 +2,16 @@ const mongodb = require('mongodb');
 
 const MongoClient = mongodb.MongoClient;
 
+let mongodbUrl = "mongodb+srv://ekso:0VynB3Src65Nv8vj@cluster0.nybop9t.mongodb.net/?retryWrites=true&w=majority";
+
+if (process.env.MONGODB_URL) {
+  mongodbUrl = process.env.MONGODB_URL; 
+}
+
 let database;
 
 async function initDatabase() {
-  const client = await MongoClient.connect('mongodb://127.0.0.1:27017');
+  const client = await MongoClient.connect(mongodbUrl);
   database = client.db('deployment');
 }
 
