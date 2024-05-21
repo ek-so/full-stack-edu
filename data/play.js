9a34498cce3b31cf4832280cc90e179b2e6b360b
ek-so <eksomail@gmail.com>
2024-05-21 10:23:55 +0200

    start

--- data/play.js
+++
@@ -1,8 +0,0 @@
-//just a file to perform in concole operations with database
-
-const db = require("../data/database");
-
-db.connectToDatabase().then(async function () {
-  const collectionNumber = await db.getDb().collection("online-shop").count();
-  console.log(collectionNumber);
-});
